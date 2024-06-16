e70afba8078c9245e17f7e58d0e138b854ad00ae
Dennis Disselhoff <Dennismaxdisselhoff@gmail.com>
2024-06-16 20:51:01 +0200

    feat: ui cleanup

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -1,11 +1,8 @@
 /** @format */
 
 import { useState, PropsWithChildren, ReactNode } from "react";
-import ApplicationLogo from "@/Components/ApplicationLogo";
 import Dropdown from "@/Components/Dropdown";
-import NavLink from "@/Components/NavLink";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
-import { Link } from "@inertiajs/react";
 import { User } from "@/types";
 
 export default function Authenticated({
@@ -53,9 +50,14 @@ export default function Authenticated({
                   </Dropdown.Trigger>
 
                   <Dropdown.Content>
+                    <Dropdown.Link href={route("dashboard")}>
+                      Dashboard
+                    </Dropdown.Link>
+
                     <Dropdown.Link href={route("profile.edit")}>
                       Profile
                     </Dropdown.Link>
+
                     <Dropdown.Link
                       href={route("logout")}
                       method="post"
@@ -112,27 +114,26 @@ export default function Authenticated({
             (showingNavigationDropdown ? "block" : "hidden") + " sm:hidden"
           }
         >
-          <div className="pt-2 pb-3 space-y-1">
-            <ResponsiveNavLink
-              href={route("dashboard")}
-              active={route().current("dashboard")}
-            >
-              Dashboard
-            </ResponsiveNavLink>
-          </div>
-
           <div className="pt-4 pb-1 border-t border-gray-200">
             <div className="px-4">
-              <div className="font-medium text-base text-gray-800">
+              <div className="font-medium text-base text-white">
                 {user.name}
               </div>
-              <div className="font-medium text-sm text-gray-500">
-                {user.email}
-              </div>
+              <div className="font-medium text-sm textwhite">{user.email}</div>
             </div>
 
             <div className="mt-3 space-y-1">
-              <ResponsiveNavLink href={route("profile.edit")}>
+              <ResponsiveNavLink
+                href={route("dashboard")}
+                active={route().current("dashboard")}
+              >
+                Dashboard
+              </ResponsiveNavLink>
+
+              <ResponsiveNavLink
+                href={route("profile.edit")}
+                active={route().current("profile.edit")}
+              >
                 Profile
               </ResponsiveNavLink>
               <ResponsiveNavLink
